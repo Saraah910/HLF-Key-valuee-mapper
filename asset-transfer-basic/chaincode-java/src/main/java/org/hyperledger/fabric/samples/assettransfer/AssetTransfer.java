415a44edf9182666ee28c3f5b83bfdfdd7ac973c
sakshi1701 <sakshiaherkar17@gmail.com>
2023-06-29 17:40:29 +0530

    First commit

--- asset-transfer-basic/chaincode-java/src/main/java/org/hyperledger/fabric/samples/assettransfer/AssetTransfer.java
+++ asset-transfer-basic/chaincode-java/src/main/java/org/hyperledger/fabric/samples/assettransfer/AssetTransfer.java
@@ -51,17 +51,28 @@ public final class AssetTransfer implements ContractInterface {
      *
      * @param ctx the transaction context
      */
+    public static class Asset {
+        public String AssetID;
+        public String Name;
+        public String Color;
+        public int Size;
+    }
+
+    public static class NewOwner {
+        public String OwnerID;
+        public String OwnerName;
+    }
+
     @Transaction(intent = Transaction.TYPE.SUBMIT)
-    public void InitLedger(final Context ctx) {
+
+    public void InitLedger(final Context ctx){
         ChaincodeStub stub = ctx.getStub();
 
-        CreateAsset(ctx, "asset1", "blue", 5, "Tomoko", 300);
-        CreateAsset(ctx, "asset2", "red", 5, "Brad", 400);
-        CreateAsset(ctx, "asset3", "green", 10, "Jin Soo", 500);
-        CreateAsset(ctx, "asset4", "yellow", 10, "Max", 600);
-        CreateAsset(ctx, "asset5", "black", 15, "Adrian", 700);
-        CreateAsset(ctx, "asset6", "white", 15, "Michel", 700);
-
+        mappingOwnerAsset(ctx, "1001", "ASSET001")
+        mappingOwnerAsset(ctx, "1002", "ASSET002")
+        mappingOwnerAsset(ctx, "1003", "ASSET003")
+        mappingOwnerAsset(ctx, "1004", "ASSET004")
+        mappingOwnerAsset(ctx, "1005", "ASSET005")
     }
 
     /**
@@ -71,169 +82,47 @@ public final class AssetTransfer implements ContractInterface {
      * @param assetID the ID of the new asset
      * @param color the color of the new asset
      * @param size the size for the new asset
-     * @param owner the owner of the new asset
-     * @param appraisedValue the appraisedValue of the new asset
      * @return the created asset
      */
+     
+
     @Transaction(intent = Transaction.TYPE.SUBMIT)
-    public Asset CreateAsset(final Context ctx, final String assetID, final String color, final int size,
-        final String owner, final int appraisedValue) {
+    public String CreateOwner(Context ctx, String ownerId, String ownerName) {
         ChaincodeStub stub = ctx.getStub();
+        NewOwner newOwner = new NewOwner();
+        newOwner.OwnerID = ownerId;
+        newOwner.OwnerName = ownerName;
 
-        if (AssetExists(ctx, assetID)) {
-            String errorMessage = String.format("Asset %s already exists", assetID);
-            System.out.println(errorMessage);
-            throw new ChaincodeException(errorMessage, AssetTransferErrors.ASSET_ALREADY_EXISTS.toString());
-        }
-
-        Asset asset = new Asset(assetID, color, size, owner, appraisedValue);
-        // Use Genson to convert the Asset into string, sort it alphabetically and serialize it into a json string
-        String sortedJson = genson.serialize(asset);
-        stub.putStringState(assetID, sortedJson);
-
-        return asset;
+        String sortedJson = genson.serialize(newOwner);
+        return ownerId;
     }
 
-    /**
-     * Retrieves an asset with the specified ID from the ledger.
-     *
-     * @param ctx the transaction context
-     * @param assetID the ID of the asset
-     * @return the asset found on the ledger if there was one
-     */
-    @Transaction(intent = Transaction.TYPE.EVALUATE)
-    public Asset ReadAsset(final Context ctx, final String assetID) {
-        ChaincodeStub stub = ctx.getStub();
-        String assetJSON = stub.getStringState(assetID);
-
-        if (assetJSON == null || assetJSON.isEmpty()) {
-            String errorMessage = String.format("Asset %s does not exist", assetID);
-            System.out.println(errorMessage);
-            throw new ChaincodeException(errorMessage, AssetTransferErrors.ASSET_NOT_FOUND.toString());
-        }
-
-        Asset asset = genson.deserialize(assetJSON, Asset.class);
-        return asset;
-    }
-
-    /**
-     * Updates the properties of an asset on the ledger.
-     *
-     * @param ctx the transaction context
-     * @param assetID the ID of the asset being updated
-     * @param color the color of the asset being updated
-     * @param size the size of the asset being updated
-     * @param owner the owner of the asset being updated
-     * @param appraisedValue the appraisedValue of the asset being updated
-     * @return the transferred asset
-     */
     @Transaction(intent = Transaction.TYPE.SUBMIT)
-    public Asset UpdateAsset(final Context ctx, final String assetID, final String color, final int size,
-        final String owner, final int appraisedValue) {
+    public String CreateAsset(Context ctx, String assetId, String name, String color, int size) {
         ChaincodeStub stub = ctx.getStub();
 
-        if (!AssetExists(ctx, assetID)) {
-            String errorMessage = String.format("Asset %s does not exist", assetID);
-            System.out.println(errorMessage);
-            throw new ChaincodeException(errorMessage, AssetTransferErrors.ASSET_NOT_FOUND.toString());
-        }
+        Asset newAsset = new Asset();
+        newAsset.AssetID = assetId;
+        newAsset.Name = name;
+        newAsset.Color = color;
+        newAsset.Size = size;
 
-        Asset newAsset = new Asset(assetID, color, size, owner, appraisedValue);
-        // Use Genson to convert the Asset into string, sort it alphabetically and serialize it into a json string
         String sortedJson = genson.serialize(newAsset);
-        stub.putStringState(assetID, sortedJson);
-        return newAsset;
+
+        return assetId;
     }
 
-    /**
-     * Deletes asset on the ledger.
-     *
-     * @param ctx the transaction context
-     * @param assetID the ID of the asset being deleted
-     */
     @Transaction(intent = Transaction.TYPE.SUBMIT)
-    public void DeleteAsset(final Context ctx, final String assetID) {
+    public void mappingOwnerAsset(Context ctx, String assetId, String ownerId) {
         ChaincodeStub stub = ctx.getStub();
-
-        if (!AssetExists(ctx, assetID)) {
-            String errorMessage = String.format("Asset %s does not exist", assetID);
-            System.out.println(errorMessage);
-            throw new ChaincodeException(errorMessage, AssetTransferErrors.ASSET_NOT_FOUND.toString());
-        }
-
-        stub.delState(assetID);
+        stub.putStringState(ownerId, assetId);
     }
 
-    /**
-     * Checks the existence of the asset on the ledger
-     *
-     * @param ctx the transaction context
-     * @param assetID the ID of the asset
-     * @return boolean indicating the existence of the asset
-     */
     @Transaction(intent = Transaction.TYPE.EVALUATE)
-    public boolean AssetExists(final Context ctx, final String assetID) {
+    public String queryAssetOwner(Context ctx, String ownerId) {
         ChaincodeStub stub = ctx.getStub();
-        String assetJSON = stub.getStringState(assetID);
-
-        return (assetJSON != null && !assetJSON.isEmpty());
+        return stub.getStringState(ownerId);
     }
 
-    /**
-     * Changes the owner of a asset on the ledger.
-     *
-     * @param ctx the transaction context
-     * @param assetID the ID of the asset being transferred
-     * @param newOwner the new owner
-     * @return the old owner
-     */
-    @Transaction(intent = Transaction.TYPE.SUBMIT)
-    public String TransferAsset(final Context ctx, final String assetID, final String newOwner) {
-        ChaincodeStub stub = ctx.getStub();
-        String assetJSON = stub.getStringState(assetID);
-
-        if (assetJSON == null || assetJSON.isEmpty()) {
-            String errorMessage = String.format("Asset %s does not exist", assetID);
-            System.out.println(errorMessage);
-            throw new ChaincodeException(errorMessage, AssetTransferErrors.ASSET_NOT_FOUND.toString());
-        }
-
-        Asset asset = genson.deserialize(assetJSON, Asset.class);
-
-        Asset newAsset = new Asset(asset.getAssetID(), asset.getColor(), asset.getSize(), newOwner, asset.getAppraisedValue());
-        // Use a Genson to conver the Asset into string, sort it alphabetically and serialize it into a json string
-        String sortedJson = genson.serialize(newAsset);
-        stub.putStringState(assetID, sortedJson);
-
-        return asset.getOwner();
-    }
-
-    /**
-     * Retrieves all assets from the ledger.
-     *
-     * @param ctx the transaction context
-     * @return array of assets found on the ledger
-     */
-    @Transaction(intent = Transaction.TYPE.EVALUATE)
-    public String GetAllAssets(final Context ctx) {
-        ChaincodeStub stub = ctx.getStub();
-
-        List<Asset> queryResults = new ArrayList<Asset>();
-
-        // To retrieve all assets from the ledger use getStateByRange with empty startKey & endKey.
-        // Giving empty startKey & endKey is interpreted as all the keys from beginning to end.
-        // As another example, if you use startKey = 'asset0', endKey = 'asset9' ,
-        // then getStateByRange will retrieve asset with keys between asset0 (inclusive) and asset9 (exclusive) in lexical order.
-        QueryResultsIterator<KeyValue> results = stub.getStateByRange("", "");
-
-        for (KeyValue result: results) {
-            Asset asset = genson.deserialize(result.getStringValue(), Asset.class);
-            System.out.println(asset);
-            queryResults.add(asset);
-        }
-
-        final String response = genson.serialize(queryResults);
-
-        return response;
-    }
+    
 }
